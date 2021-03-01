9e58664a5bb2f3363ec383e46e775f5bb6e797d9
Battuul <bak.battuul@gmail.com>
2021-03-01 09:58:20 -0600

    signup fix

---
+++ helloworld.java
@@ -0,0 +1,23 @@
+package miu.cs472;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Simplest")
+public class SimplestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Hello world!</title></head><body>");
+		out.print("<p>Hello world!</p>");
+		out.print("</body></html>");
+	}
+}
